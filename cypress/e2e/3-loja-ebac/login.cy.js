5e73d42021018a4cf26401ec4edb7a8c83f6a10b
Elisiar Soares <elisiars9@gmail.com>
2024-11-04 19:52:33 -0300

    Primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,17 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+    
+    it ('Deeve fazeer login com sucesso', () => {
+
+        cy.visit("http://lojaebac.ebaconline.art.br/minha-conta/")
+        cy.get('#username').type('elisiarQA@teste.com.br')
+        cy.get('#password').type('CGHXjcr2qmHjyLe')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, elisiarqa (não é elisiarqa? Sair')
+    })
+
+
+
+
+})
