5833c311fdd5d733470ecc0792ee7a5d3942465e
Elisiar Soares <elisiars9@gmail.com>
2024-11-12 18:25:14 -0300

    Testes automatizados

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,15 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+        beforeEach(() => {
+            cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        });
+        it('Deve selecionar um produto da lista', () => {
+            cy.get('.product-block ')
+        .eq(4)
+            .click()
+            cy.get('#tab-title-description > a').should ('contain' , 'Descrição') 
+        });
+   
+    
+});
